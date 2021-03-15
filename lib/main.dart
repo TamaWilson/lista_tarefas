d43da43672484be072c4a9ef478ae369c6edb6c8
TamaWilson <wilson_tama@hotmail.com>
2021-03-15 20:28:10 -0300

    Atualizando versão do Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,221 +1,113 @@
-import 'dart:convert';
-import 'dart:io';
-
-import "package:flutter/material.dart";
-import "package:path_provider/path_provider.dart";
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Lista de Tarefas",
-    home: Home(),
-  ));
+  runApp(MyApp());
 }
 
-class Home extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
-  _HomeState createState() => _HomeState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
-class _HomeState extends State<Home> {
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final _toDoController = TextEditingController();
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
 
-  List _toDoList = [];
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
-  Map<String, dynamic> _lastRemoved;
-  int _lastPos;
+  final String title;
 
   @override
-  void initState() {
-    super.initState();
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-    _readData().then((data) {
-      setState(() {
-        _toDoList = json.decode(data);
-      });
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  Future<Null> _refreshData() async {
-    await Future.delayed(Duration(seconds: 1));
+  void _incrementCounter() {
     setState(() {
-      _toDoList.sort((a, b) {
-        if (a["ok"] && !b["ok"]) {
-          return 1;
-        } else if (!a["ok"] && b["ok"]) {
-          return -1;
-        } else {
-          return 0;
-        }
-      });
-
-      _saveData();
-    });
-  }
-
-  void _addToDo() {
-    setState(() {
-      Map<String, dynamic> newToDo = new Map();
-      newToDo["title"] = _toDoController.text;
-      _toDoController.clear();
-      newToDo["ok"] = false;
-      _toDoList.add(newToDo);
-
-      _saveData();
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Lista de Tarefas"),
-          backgroundColor: Colors.blueAccent,
-          centerTitle: true,
-          actions: <Widget>[
-            FlatButton(
-              child: Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-              onPressed: _clearAll,
-            )
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
           ],
         ),
-        body: Column(children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                    child: Form(
-                  key: _formKey,
-                  child: TextFormField(
-                    controller: _toDoController,
-                    decoration: InputDecoration(
-                      labelText: "Nova Tarefa",
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return "Insira um nome para a tarefa";
-                      }
-                    },
-                  ),
-                )),
-                Container(
-                  padding: EdgeInsets.only(left: 5.0),
-                  width: 100.0,
-                  height: 60.0,
-                  child: RaisedButton(
-                    color: Colors.blueAccent,
-                    child: Text(
-                      "Adicionar",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14.0),
-                    ),
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        _addToDo();
-                      }
-                    },
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: RefreshIndicator(
-                onRefresh: _refreshData,
-                child: ListView.builder(
-                    padding: EdgeInsets.only(top: 10.0),
-                    itemCount: _toDoList.length,
-                    itemBuilder: buildItem)),
-          ),
-        ]));
-  }
-
-  Widget buildItem(context, index) {
-    return Dismissible(
-      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-      background: Container(
-          color: Colors.red,
-          child: Align(
-            alignment: Alignment(-0.9, 0.0),
-            child: Icon(
-              Icons.delete,
-              color: Colors.white,
-            ),
-          )),
-      direction: DismissDirection.startToEnd,
-      child: CheckboxListTile(
-        title: Text(_toDoList[index]["title"]),
-        value: _toDoList[index]["ok"],
-        secondary: CircleAvatar(
-          child:
-              Icon(_toDoList[index]["ok"] ? Icons.check_circle : Icons.error),
-        ),
-        onChanged: (checked) {
-          setState(() {
-            _toDoList[index]["ok"] = checked;
-            _saveData();
-          });
-        },
       ),
-      onDismissed: (direction) {
-        setState(() {
-          _lastRemoved = Map.from(_toDoList[index]);
-          _lastPos = index;
-          _toDoList.removeAt(index);
-          _saveData();
-
-          final snack = SnackBar(
-              content: Text("Tarefa \"${_lastRemoved["title"]}\" removida"),
-              action: SnackBarAction(
-                  label: "Desfazer",
-                  onPressed: () {
-                    setState(() {
-                      _toDoList.insert(_lastPos, _lastRemoved);
-                      _saveData();
-                    });
-                  }),
-              duration: Duration(seconds: 3));
-
-          Scaffold.of(context).removeCurrentSnackBar();
-          Scaffold.of(context).showSnackBar(snack);
-        });
-      },
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  void _clearAll() {
-    setState(() {
-      List fullList = _toDoList;
-
-      _toDoList.clear();
-      _saveData();
-    });
-  }
-
-  Future<File> _getFile() async {
-    final directory = await getApplicationDocumentsDirectory();
-    return File("${directory.path}/data.json");
-  }
-
-  Future<File> _saveData() async {
-    String data = json.encode(_toDoList);
-    final file = await _getFile();
-    return file.writeAsString(data);
-  }
-
-  Future<String> _readData() async {
-    try {
-      final file = await _getFile();
-      return file.readAsString();
-    } catch (e) {
-      return null;
-    }
   }
 }
